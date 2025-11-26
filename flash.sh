67af680b5500329825295d8e8ee03d985f114939
www-e-ventures <the@e.ventures>
2025-11-26 10:18:04 -0800

    custom boot

--- flash.sh
+++ flash.sh
@@ -11,7 +11,7 @@ echo "--------------------------------------"
 pio run -e t-deck-tft --target upload
 pio run -e t-deck-tft --target uploadfs
 
-# pio device monitor
+#pio device monitor
 
 echo "--------------------------------------"
 echo "   Flash Complete!                    "
